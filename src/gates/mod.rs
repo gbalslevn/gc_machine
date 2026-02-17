95c7c207d074534790be799ef569189a4f199e08
MathiasKrogh <mathias@bondekrogh.dk>
2026-02-17 01:24:48 +0100

    Point and Permute gates initialized, still needs testing

--- src/gates/mod.rs
+++ src/gates/mod.rs
@@ -1,2 +1,3 @@
 pub mod gates;
 pub mod original_gates;
+mod point_and_permute_gates;
---
+++ src/gates/point_and_permute_gates.rs
@@ -0,0 +1,31 @@
+use std::ops::{BitXor, Shl};
+use num_bigint::BigUint;
+use crate::crypto_utils;
+use rand::{thread_rng};
+use rand::seq::SliceRandom;
+use crate::gates::gates::Gates;
+
+pub struct PointAndPermuteGates;
+
+impl Gates for PointAndPermuteGates {
+    fn get_garbled_gate(tt : &[(BigUint, BigUint, BigUint); 4], gate_id: &BigUint) -> Vec<BigUint> {
+        let mut table = vec![BigUint::from(0u8); 4];
+        // Creating symmetric key from left input, right input and gate id then encrypting the tt output with the key
+        for (il, ir, out) in tt {
+            let key = crypto_utils::gc_kdf_128(il, ir, gate_id);
+            let ct = key.bitxor(out);
+            let pos = get_position(il, ir);
+            table[pos]= ct;
+        }
+        table
+    }
+}
+
+pub fn get_position(il: &BigUint, ir: &BigUint) -> usize {
+    let l = il.bit(0) as usize;
+    let r = ir.bit(0) as usize;
+    l * 2 + r
+}
+
+
+
