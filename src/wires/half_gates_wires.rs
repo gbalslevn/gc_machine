1ca7dba6d75091a04bc21707bce19877e855d079
Mathias Krogh <mathias@bondekrogh.dk>
2026-02-25 17:13:44 +0100

    Needs index field

---
+++ src/wires/half_gates_wires.rs
@@ -0,0 +1,86 @@
+use num_bigint::{BigUint, ToBigUint};
+use crate::gates::gates::GateType;
+use crate::wires::wires::{Wire, Wires};
+use crate::crypto_utils::{gc_kdf_128, generate_label_lsb, generate_label, gc_kdf_hg};
+
+pub struct FreeXORWires {
+    pub delta: BigUint,
+    pub index: BigUint,
+}
+
+impl FreeXORWires {
+    pub fn delta(&self) -> &BigUint {
+        &self.delta
+    }
+}
+
+impl Wires for FreeXORWires {
+    fn new() -> Self {
+        let delta = generate_label_lsb(true); // to ensure point and permute holds
+        let index = 0.to_biguint().unwrap();
+        FreeXORWires { delta, index }
+    }
+
+    fn generate_input_wire(&self) -> Wire {
+        let delta = self.delta();
+        let w0 = generate_label();
+        let w1 = &w0 ^ delta;
+        Wire::new(w0, w1)
+    }
+    fn generate_output_wire(&self, wi: &Wire, wj: &Wire, gate: &GateType, gate_id: &BigUint) -> Wire {
+        let delta = self.delta();
+        match gate {
+            GateType::AND=>generate_and_wires(delta, &wi, &wj, gate_id),
+            GateType::XOR=>generate_xor_wires(delta, &wi, &wj, gate_id),
+        }
+    }
+}
+
+pub fn generate_and_wires(delta: &BigUint, wi: &Wire, wj: &Wire, gate_id: &BigUint) -> Wire {
+    let w0c;
+    let w1c;
+    let pa = wi.w0().bit(0);
+    let pb = wj.w0().bit(0);
+    let j0 = self.index.increment();
+    let j1 = index.increment();
+    let tg;
+    if pb {
+        tg = gc_kdf_hg(&wi.w0(), j0) ^ gc_kdf_hg(&wi.w1(), j0) ^ delta;
+    } else {
+        tg = gc_kdf_hg(&wi.w0(), j0) ^ gc_kdf_hg(&wi.w1(), j0);
+    }
+    let wg;
+    if pa {
+        wg = gc_kdf_hg(&wi.w0(), j0) ^ tg;
+    } else {
+        wg = gc_kdf_hg(&wi.w0(), j0)
+    }
+
+    let te = gc_kdf_hg(&wj.w0(), j1) ^ gc_kdf_hg(&wj.w1(), j1) ^ wi.w0();
+    let we;
+    if pb {
+        we = gc_kdf_hg(&wj.w0(), j1) ^ te ^ wi.w0();
+    } else {
+        we = gc_kdf_hg(&wj.w0(), j1)
+    }
+    w0c = wg ^ we;
+    w1c = &w0c ^ delta;
+
+    Wire::new(w0c, w1c)
+}
+pub fn generate_xor_wires(delta: &BigUint, wi: &Wire, wj: &Wire, _gate_id: &BigUint) -> Wire {
+    let w0c = wi.w0() ^ wj.w0();
+    let w1c = &w0c ^ delta.clone();
+    Wire::new(w0c, w1c)
+}
+
+pub fn get_00_wire(wi: &Wire, wj: &Wire, gate_id: &BigUint) -> BigUint {
+    for left in [&wi.w0(), &wi.w1()] {
+        for right in [&wj.w0(), &wj.w1()] {
+            if !left.bit(0) && !right.bit(0) {
+                return gc_kdf_128(&left, &right, gate_id)
+            }
+        }
+    }
+    panic!("Couldn't find where both wires lsb was 0");
+}
