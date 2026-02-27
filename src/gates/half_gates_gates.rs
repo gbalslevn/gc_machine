7ba0f36315e3e44f4f3b550c707efa8f6658f12d
au662052 <au662052@uni.au.dk>
2026-02-27 12:17:56 +0100

    start impl of half-gates-ciphertexts

--- src/gates/half_gates_gates.rs
+++ src/gates/half_gates_gates.rs
@@ -1,4 +1,5 @@
 use num_bigint::BigUint;
+use crate::crypto_utils::gc_kdf_hg;
 use crate::gates::gates::{Gate, GateType, Gates};
 use crate::wires::wires::{Wire, Wires};
 
@@ -13,10 +14,9 @@ impl<W: Wires> Gates<W> for HalfGatesGates<W> {
 
     fn generate_gate(&self, gate: GateType, wi: Wire, wj: Wire, gate_id: BigUint) -> Gate {
         let wo = self.wires.generate_output_wire(&wi, &wj, &gate, &gate_id);
-        let tt = self.get_tt(&wi, &wj, &wo, &gate);
         match gate {
             GateType::AND=> {
-                let table = generate_and_table(&tt, &gate_id);
+                let table = generate_and_table(&wi, &wj, &gate_id);
                 Gate { gate_id, gate_type: GateType::AND, table, wi: wi, wj: wj, wo: wo }
             }
             GateType::XOR=> {
@@ -24,8 +24,22 @@ impl<W: Wires> Gates<W> for HalfGatesGates<W> {
             }
         }
     }
+}
 
-    fn generate_and_table(tt: &[(BigUint, BigUint, BigUint); 4], gate_id: &BigUint) -> Vec<BigUint> {
-        let mut table = vec![BigUint::from(0u8); 2];
+fn generate_and_table(wi: &Wire, wj: &Wire, gate_id: &BigUint) -> Vec<BigUint> {
+    let mut table = vec![BigUint::from(0u8); 2];
+    let tg;
+    let te;
+    let index_j;
+    let index_j_prime;
+    let delta = wi.w0() ^ wi.w1(); // could be given as argument through wiresGen struct
+    tg = gc_kdf_hg(wi.w0(), index_j) ^ gc_kdf_hg(wi.w1(), index_j) ^
+}
+
+fn number_mult_bitstring(boolean: &bool, bitstring: &BigUint) -> BigUint {
+    if (boolean) {
+        bitstring.clone()
+    } else {
+        BigUint::from(0u8);
     }
 }
